fd2a4b2cc726a72b4d115008799467fe7b688f74
Riverside Rocks <trent@riverside.rocks>
2020-10-05 09:02:35 -0400

    Add notice

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,21 @@
+<?php
+/*======================================================================
+Copyright 2020, Riverside Rocks and the DUDB Authors
+
+Licensed under the the Apache License v2.0 (the "License")
+
+You may get a copy at
+https://apache.org/licenses/LICENSE-2.0.txt
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+========================================================================*/
+
+
+?>
 <!DOCTYPE html>
 <head>
 <link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.0/css/halfmoon-variables.min.css" rel="stylesheet" />
