1ea60d108cbad8f52b05f60dd1588016e43cc3e8
Riverside Rocks <trent@riverside.rocks>
2020-10-05 11:30:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -16,4 +16,5 @@ limitations under the License.
 
 include "includes/header.php";
 ?>
-<h1>Welcome to the Dangerous Discord User Database!</h1>
+<h1>Remove Danerous Users From Your Server - Fast</h1>
+<h3>With our robust API, you will never have to worry about raids again</h3>
