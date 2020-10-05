b27ad5e1022139c0d400d9283b51c8466dc63cfb
Riverside Rocks <trent@riverside.rocks>
2020-10-05 08:59:02 -0400

    Getting started!

---
+++ includes/header.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.0/css/halfmoon-variables.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/halfmoon@1.1.0/js/halfmoon.min.js"></script>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include "includes/header.php";
+?>
+<h1>Welcome to the Dangerous Discord User Database</h1>
+<i>The place to report harmful discord users</i>
