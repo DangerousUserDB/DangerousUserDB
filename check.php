5126f0e4666500070ed8e6ae9e64b2c6766b45b4
Riverside Rocks <trent@riverside.rocks>
2020-10-06 11:51:33 -0400

    Update check.php

--- check.php
+++ check.php
@@ -66,23 +66,13 @@ if ($result->num_rows > 0) {
     }
 }
 
-$message = "Failed to load the data. Please reload.";
 
-if($times == "0"){
-    $symbol = '<i class="fas fa-check-circle" style="color:green;font-size:18px;"></i>';
-    $message = "Good news, this user looks fine.";
-}
-
-if($times > "1" && $times < "3"){
-    $symbol = '<i class="fas fa-exclamation-triangle" style="color:yellowfont-size:18px;"></i>';
-    $message = "Keep your eyes on this user, we have at least one report.";
-}else if($times > "3" && $times < "5"){
-    $symbol = '<i class="fas fa-exclamation-circle" style="color:redfont-size:18px;"></i>';
-    $message = "This user is likey a malicous user, they have more than 3 reports.";
-}else if($times > "5"){
-    $symbol = '<i class="fas fa-radiation-alt" style="color:red"></i>';
-    $message = "This user is a malicous user, remove them from your server as soon as possible.";
-
+if($times !== "0"){
+    $symbol = '<i class="fas fa-radiation-alt" style="color:red;font-size:18px;"></i>'; 
+    $message = "Warning: We have recived ${times} report(s) about this user.";
+}else{
+    $symbol = '<i class="fas fa-check-circle" style="color:green;font-size:18px;"></i>'; 
+    $message = "All clear! Nothing looks wrong!";
 }
 
 
