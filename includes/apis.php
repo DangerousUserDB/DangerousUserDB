24b0eeebdd3fbd9dcb11470a691b816c0dfc36f1
Riverside Rocks <trent@riverside.rocks>
2020-10-09 15:21:03 -0400

    Score Service

--- includes/apis.php
+++ includes/apis.php
@@ -14,3 +14,37 @@ limitations under the License.
 header("Content-type: application/json");
 
 require 'vendor/autoload.php';
+
+$numbers = array(
+    "1" => "14%",
+    "2" => "24%",
+    "3" => "38%"
+)
+
+/*===============================================
+
+Reports alone are not really the best way to tell how 
+abusive a user is. The best way is to see how long ago 
+the last report was and the "density" of reports. The
+function score() calculates a score from 0-100. It
+takes three arguments, the first being the total number
+of unique reports, the second being the first time a report
+was sent in, and the third being the last time a report
+was sent in. This uses PHP's default time function which
+returns times in UTC.
+
+================================================*/
+
+function score($number, $first_epoch, $last_epoch)
+{
+    $timeframe = $last_epoch - $first_epoch;
+
+    if(!in_array($number, $numbers))
+    {
+        $abuse = $timeframe/$score * 10;
+    }
+    else
+    {
+        $abuse = $numbers[$number];
+    }
+}
