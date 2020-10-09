84bb4cf900d4a616cab70fe4327ebd3ae0ab1e5d
Riverside Rocks <trent@riverside.rocks>
2020-10-08 20:06:14 -0400

    Update login.php

--- login.php
+++ login.php
@@ -52,10 +52,10 @@ if ($oauth2->isRedirected() === false) { // Did the client already logged in ?
         } else {
             print_r($answer);
             //$answer["username"] = $_SESSION["discord_username"];
-            //$answer["id"] = $_SESSION["discord_id"];
-            //echo $_SESSION["discord_username"];
-            //header("Location: /");
-            //die();
+            $answer["id"] = $_SESSION["discord_id"];
+            echo $_SESSION["discord_username"];
+            header("Location: /");
+            die();
         }
 
         echo '<br/><br/>';
