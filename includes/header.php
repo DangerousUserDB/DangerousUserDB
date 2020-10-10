9185b31b5e7daf7830319e4532427c5605176a05
Riverside Rocks <trent@riverside.rocks>
2020-10-10 09:30:00 -0400

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -43,7 +43,7 @@ echo $font_awesome_kit;
 <link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.0/css/halfmoon-variables.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/halfmoon@1.1.0/js/halfmoon.min.js"></script>
 </head>
-<body>
+<body onload="halfmoon.toggleDarkMode()">
 <center>
 <div class="page-wrapper with-navbar">
     <!-- Navbar (immediate child of the page wrapper) -->
