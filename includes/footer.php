c2c39a8701984e433fc8c09169f25b1c97298ea6
Riverside Rocks <trent@riverside.rocks>
2020-10-10 17:57:42 -0400

    Add a bit more spacing on the bottom
    Removing spacing on the top

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
-<br><br>
+<br>
 <center>
 <?php echo date("Y", time()) . " - <a href='//riverside.rocks'>Riverside Rocks</a> - <a href=<a href='https://github.com/DangerousUserDB/DangerousUserDB'>Github</a>";
+?>
+<br>
