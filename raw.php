cc14297d797d5ad3bc8598989dda95e3ba4c53f1
Riverside Rocks <trent@riverside.rocks>
2020-10-11 07:35:32 -0400

    Raw (beta)

---
+++ raw.php
@@ -0,0 +1,46 @@
+<?php
+/*======================================================================
+Copyright 2020, Riverside Rocks and the DUDB Authors
+
+Licensed under the the Apache License v2.0 (the "License")
+
+You may get a copy at
+https://apache.org/licenses/LICENSE-2.0.txt
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+========================================================================*/
+
+include "includes/header.php";
+include "includes/apis.php";
+
+
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['MYSQL_SERVER'];
+$username = $_ENV["MYSQL_USERNAME"];
+$password = $_ENV["MYSQL_PASSWORD"];
+$dbname = $_ENV["MYSQL_DATABASE"];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if($_SESSION["discord_username"] == ""){
+    die("To prevent abuse, please sign in.");
+}
+
+$sql = "SELECT * FROM reports WHERE discord_id='${sql_discord}'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        
+    }
+}
