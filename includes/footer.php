8a86a0309bfb827be96b7d5cffaac39a9b3a80f7
Riverside Rocks <trent@riverside.rocks>
2020-10-11 18:57:21 -0400

    Add analytics

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
+<script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+<noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt=""/></noscript>
 <br>
 <center>
 <?php echo date("Y", time()) . " - <a href='//riverside.rocks'>Riverside Rocks</a> - <a href=<a href='https://github.com/DangerousUserDB/DangerousUserDB'>Github</a>";
