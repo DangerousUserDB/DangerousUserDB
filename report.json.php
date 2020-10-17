08579e592c195830f35ac3119b042e7f54e7da19
Riverside Rocks <trent@riverside.rocks>
2020-10-17 12:29:24 -0400

    Add a semicolon

--- report.json.php
+++ report.json.php
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ========================================================================*/
 
-header("Content-type: application/json")
+header("Content-type: application/json");
 
 include "includes/apis.php";
 
