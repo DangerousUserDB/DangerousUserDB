329dbe51c798085579298c232f27afa186e0ea85
Riverside Rocks <trent@riverside.rocks>
2020-10-19 11:43:36 -0400

    Decode it (for real this time)

--- report.json.php
+++ report.json.php
@@ -89,7 +89,7 @@ if(isset($_POST["id"])){
     $cat = $conn -> real_escape_string(xss("api-report"));
     $epoch = time();
     $details = $conn -> real_escape_string(xss($_POST["details"]));
-    $apis = json_decode(file_get_contents("https://discord.riverside.rocks/check.json.php?id=" . $discord_reporter));
+    $apis = json_decode(file_get_contents("https://discord.riverside.rocks/check.json.php?id=" . $discord_reporter), true);
     $discord_username = $conn -> real_escape_string(xss($apis["username"]));
     // Note, we don't halt the request here if there are not details. Details are
     // not required for a report.
