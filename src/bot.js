2c68032fe2d561a56d6f262ef319580449f7945b
Riverside Rocks <trent@riverside.rocks>
2020-10-20 11:07:15 -0400

    Change up the error a tiny bit

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,14 @@
 function alertBot() {
     halfmoon.toggleDarkMode()
     Swal.fire({
-        title: 'New! Discord Bot',
-        text: 'Get our Discord Bot! <a href="https://discord.com/oauth2/authorize?client_id=764485265775263784&scope=bot&permissions=3072">Click here</a>',
-      })
+      title: '<strong>New! Discord Bot</strong>',
+      icon: 'info',
+      html: 'Get our Discord Bot! <a href="https://discord.com/oauth2/authorize?client_id=764485265775263784&scope=bot&permissions=3072">Click here</a>',
+      showCloseButton: true,
+      focusConfirm: false,
+      confirmButtonText:
+        '<i class="fa fa-thumbs-up"></i> Great!',
+      confirmButtonAriaLabel: 'Thumbs up, great!',
+    })
      // Script also toggles HalfMoon's Dark Theme. In a future update, this will be configurable on the dashboard.
 }
