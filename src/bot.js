96d77cac811e1ec16378ad9c9b3402cb1587b551
Riverside Rocks <trent@riverside.rocks>
2020-10-20 11:14:14 -0400

    Update message on first view

--- src/bot.js
+++ src/bot.js
@@ -3,12 +3,11 @@ function alertBot() {
     Swal.fire({
       title: '<strong>New! Discord Bot</strong>',
       icon: 'info',
-      html: 'Get our Discord Bot! <a href="https://discord.com/oauth2/authorize?client_id=764485265775263784&scope=bot&permissions=3072">Click here</a>',
-      showCloseButton: true,
+      html: 'Get our Discord Bot! <a href="https://discord.com/oauth2/authorize?client_id=764485265775263784&scope=bot&permissions=3072" target="_blank">Click here</a>',
       focusConfirm: false,
       confirmButtonText:
         '<i class="fa fa-thumbs-up"></i> Great!',
-      confirmButtonAriaLabel: 'Thumbs up, great!',
+      confirmButtonAriaLabel: 'Sounds good!',
     })
      // Script also toggles HalfMoon's Dark Theme. In a future update, this will be configurable on the dashboard.
 }
