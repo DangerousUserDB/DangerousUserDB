26a77a2b2c70d47a5f601af52e1107d02035cff4
Riverside Rocks <trent@riverside.rocks>
2020-10-20 11:00:55 -0400

    Change order of bot script

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,8 @@
 function alertBot() {
+    halfmoon.toggleDarkMode()
     Swal.fire({
         title: 'New! Discord Bot',
         text: 'Get our Discord Bot! <a href="https://discord.com/oauth2/authorize?client_id=764485265775263784&scope=bot&permissions=3072">Click here</a>',
       })
-    halfmoon.toggleDarkMode() // Script also toggles HalfMoon's Dark Theme. In a future update, this will be configurable on the dashboard.
+     // Script also toggles HalfMoon's Dark Theme. In a future update, this will be configurable on the dashboard.
 }
