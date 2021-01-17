225cbaea7f1c0334ec619e40e74da6136e9e944d
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2021-01-17 11:38:10 -0500

    i hate cors

--- check.json.php
+++ check.json.php
@@ -11,6 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ========================================================================*/
 header("Content-type: application/json");
+header("Access-Control-Allow-Origin: *");
 
 
 include "includes/apis.php";
