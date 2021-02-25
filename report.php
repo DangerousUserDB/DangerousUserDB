480a3fb95a154a79746364bf1e635d0b7f933ce2
David <62346025+aboutDavid@users.noreply.github.com>
2021-02-25 17:03:29 -0500

    Add "other" and "harassment" report type + mod ads

--- report.php
+++ report.php
@@ -68,10 +68,12 @@ if(! $_SESSION["discord_username"]){
     <label for="languages" class="required">Abuse Type</label>
     <select class="form-control" id="cat" name="cat" multiple="multiple" required="required" size="5">
       <option value="spam">Spam</option>
-      <option value="mass-ads">Mass Ads</option>
+      <option value="mass-ads">Mass Advertiements</option>
       <option value="trolling">Trolling</option>
       <option value="raid">Raid</option>
       <option value="grabbers">IP Grabbers</option>
+      <option value="harrassment">Harrassment</option>
+      <option value="other">Other</option>
     </select>
   </div>
 
