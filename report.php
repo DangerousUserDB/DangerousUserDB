9885f9576cfeb207b7b2f65cb70e037866aa20c6
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2021-04-03 08:05:11 -0400

    Debug captcha failure

--- report.php
+++ report.php
@@ -105,73 +105,71 @@ if(isset($_POST["id"])){
     $response = curl_exec($verify);
     $responseData = json_decode($response, true);
     if($responseData["success"]) {
-        
-    /*======================
-    |   Define Variables   |
-    ======================*/
-    $discord_id = $conn -> real_escape_string(xss($_POST["id"]));
-    if(isset($_SESSION["discord_id"])){
-      $reporter_id = $conn -> real_escape_string(xss($_SESSION["discord_id"]));
+
+        /*======================
+        |   Define Variables   |
+        ======================*/
+        $discord_id = $conn -> real_escape_string(xss($_POST["id"]));
+        if(isset($_SESSION["discord_id"])){
+          $reporter_id = $conn -> real_escape_string(xss($_SESSION["discord_id"]));
+        }else{
+          $reporter_id = "0";
+        }
+        if($_SESSION["discord_username"] == ""){
+          $reporter_username = "Anonymous";
+        }else{
+          $reporter_username = $conn -> real_escape_string(xss($_SESSION["discord_username"]));
+        }
+
+        $cat = $conn -> real_escape_string(xss($_POST["cat"]));
+        /*====================
+        | Define Catagories  |
+        ====================*/
+        $cats = array(
+          "spam" => "0",
+          "trolling" => "0",
+          "mass-ads" => "0",
+          "grabbers" => "0",
+          "raid" => "0",
+          "harrassment" => "0",
+          "other" => "0"
+        );
+        if($cats[$cat] !== "0"){
+          die("<br>400: Bad Request<br>");
+        }
+        $epoch = time();
+        $details = $conn -> real_escape_string(xss($_POST["details"]));
+        if (!ctype_digit($discord_id)) {
+            die("<br>Sorry, but that doesn't look like a valid discord ID. Please try again.");
+        }
+        if(!isset($discord_id)){
+          die("Bad Request");
+        }
+        $discord_token = $_ENV['BOT_TOKEN'];
+
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => "https://discord.com/api/v8/users/${discord_id}",
+            CURLOPT_USERAGENT => 'Dangerous User DB'
+        ]);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            "Authorization: Bot ${discord_token}",
+        ));
+        $resp = curl_exec($curl);
+        curl_close($curl);
+
+        $api = json_decode($resp, true);
+        if($api["username"] == ""){
+            die("<br><br>User does not exsist. Perhaps they deleted their account?");
+        }
+
+        $sql = "INSERT INTO reports (discord_id, reporter_discord_id, reporter_discord_username, cat, details, epoch) VALUES ('${discord_id}', '${reporter_id}', '${reporter_username}', '${cat}', '${details}', '${epoch}')";
+        $result = $conn->query($sql);
+        header("Location: /check?id=${discord_id}");
+        die();
     }else{
-      $reporter_id = "0";
-    }
-    if($_SESSION["discord_username"] == ""){
-      $reporter_username = "Anonymous";
-    }else{
-      $reporter_username = $conn -> real_escape_string(xss($_SESSION["discord_username"]));
-    }
-
-    $cat = $conn -> real_escape_string(xss($_POST["cat"]));
-    /*====================
-    | Define Catagories  |
-    ====================*/
-    $cats = array(
-      "spam" => "0",
-      "trolling" => "0",
-      "mass-ads" => "0",
-      "grabbers" => "0",
-      "raid" => "0",
-      "harrassment" => "0",
-      "other" => "0"
-    );
-    if($cats[$cat] !== "0"){
-      die("<br>400: Bad Request<br>");
-    }
-    $epoch = time();
-    $details = $conn -> real_escape_string(xss($_POST["details"]));
-    if (!ctype_digit($discord_id)) {
-        die("<br>Sorry, but that doesn't look like a valid discord ID. Please try again.");
-    }
-    if(!isset($discord_id)){
-      die("Bad Request");
-    }
-    $discord_token = $_ENV['BOT_TOKEN'];
-
-
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => "https://discord.com/api/v8/users/${discord_id}",
-        CURLOPT_USERAGENT => 'Dangerous User DB'
-    ]);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-        "Authorization: Bot ${discord_token}",
-    ));
-    $resp = curl_exec($curl);
-    curl_close($curl);
-
-    $api = json_decode($resp, true);
-    if($api["username"] == ""){
-        die("<br><br>User does not exsist. Perhaps they deleted their account?");
-    }
-    
-    $sql = "INSERT INTO reports (discord_id, reporter_discord_id, reporter_discord_username, cat, details, epoch) VALUES ('${discord_id}', '${reporter_id}', '${reporter_username}', '${cat}', '${details}', '${epoch}')";
-    $result = $conn->query($sql);
-    header("Location: /check?id=${discord_id}");
-    die();
-    }else{
-        
         die("Whoops, could you try the captcha again?");
     }
-
     include "includes/footer.php";
