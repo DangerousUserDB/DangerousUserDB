a5c75d9ec53abbe58a6e3a1e724722ee01d09449
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2021-04-15 22:16:24 -0400

    Output reporter locale

--- check.php
+++ check.php
@@ -158,11 +158,13 @@ if($times == "0"){
     $reqid = $conn -> real_escape_string(xss($_GET["id"]));
     $sql = "SELECT * FROM reports WHERE discord_id='${reqid}' ORDER BY epoch DESC";
     $result = $conn->query($sql);
+    
 
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
             $t = $row["epoch"];
             echo "<tr>";
+      echo "<th><img src='https://cdn.glitch.com/2bf9c0be-400b-44c0-a3f7-277cfc7822f4%2Fblank.gif?v=1618519578835' class='flag flag-" . strtolower($row["locale"]) . "'/></th>";
       echo "<th>" . $row["reporter_discord_username"] . "</th>";
       echo "<td><span class='badge badge-danger'>" . $row["cat"] . "</span></td>";
       echo "<td>" . $row["details"] . "</td>";
