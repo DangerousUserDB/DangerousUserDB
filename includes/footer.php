abf06ada03182a50eda7bb99f4bfc41b6e02fbd3
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2021-06-20 10:26:24 -0400

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,15 @@
 <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
 <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt=""/></noscript>
 <br>
+
 <center>
-<?php echo date("Y", time()) . " - <a href='//riverside.rocks'>Riverside Rocks</a> - <a href='https://github.com/DangerousUserDB/DangerousUserDB'>Github</a>";
+<?php
+    if($r_discord_username){
+    echo "<title>${r_discord_username} - Dangerous User Database</title>";
+  }else{
+  echo "<title>" . createTitleFromURI($_SERVER['REQUEST_URI']) . " - Dangerous User Database</title>";
+  }
+  
+  echo date("Y", time()) . " - <a href='//riverside.rocks'>Riverside Rocks</a> - <a href='https://github.com/DangerousUserDB/DangerousUserDB'>Github</a>";
 ?>
 <br>
