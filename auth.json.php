1059c69c6d4729b28c2c2793568fe2d12db4173d
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2021-06-28 13:28:13 -0400

    Update auth.json.php

--- auth.json.php
+++ auth.json.php
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ========================================================================*/
 
-include "includes/header.php";
+include "includes/apis.php";
 
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
